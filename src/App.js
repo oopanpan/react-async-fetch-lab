bf1fd87b0ee1c6c4826da79cc0288d2144acfc24
oopanpan <oopanpan@gmail.com>
2021-03-18 15:36:42 -0700

    commit tree 78a04039d5e033cda8a2010bf151cdfd04c17dd8

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export class App extends Component {
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(r => r.json())
+        .then(console.log)
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default App
